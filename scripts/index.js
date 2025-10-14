dd4d9ebfbde9476e4c285317429edb2cb0a1cc3b
Rhett Bauchop <rhett.bauchop@gmail.com>
2025-10-14 17:52:37 +0800

    Moved Functions into index.js and theme.js to reduce repeated code across all html pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// index.js - All scripts for index.html
+// index.js - All scripts for index.html (and one for history.html)
 
 window.onscroll = function () {
     scrollRotate();
@@ -12,4 +12,59 @@ function scrollRotate(){
     image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
     }
     // image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+}
+
+// Popup Function
+function popupFunc(){
+    var x = document.getElementById("meetingPopup");
+    var y = document.getElementById("meetingPopupButton");
+    if(x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+}
+
+// History Page Tab Switcher
+function changeHistoryTab(){
+    const hash = location.hash
+    var ch = document.getElementById("Club History");
+    var pe = document.getElementById("Past Executives");
+    var lm = document.getElementById("Life Members");
+
+    var chHeading = document.getElementById("clubHistoryHeading");
+    var peHeading = document.getElementById("pastExecsHeading");
+    var lmHeading = document.getElementById("lifeMembersHeading");
+
+    if(hash === "#clubHistory"){
+        ch.style.display = "block";
+        pe.style.display = "none";
+        lm.style.display = "none";
+
+        chHeading.classList.add("headingBorder");
+
+        peHeading.classList.remove("headingBorder");
+        lmHeading.classList.remove("headingBorder");
+    }
+    if(hash === "#pastExecs"){
+        ch.style.display = "none";
+        pe.style.display = "block";
+        lm.style.display = "none";
+
+        peHeading.classList.add("headingBorder");
+
+        chHeading.classList.remove("headingBorder");
+        lmHeading.classList.remove("headingBorder");
+    }
+    if(hash === "#lifeMembers"){
+        ch.style.display = "none";
+        pe.style.display = "none";
+        lm.style.display = "block";
+
+        lmHeading.classList.add("headingBorder");
+
+        peHeading.classList.remove("headingBorder");
+        chHeading.classList.remove("headingBorder");
+    }
 }
---
+++ scripts/theme.js
@@ -0,0 +1,50 @@
+function setTheme(){
+    const saved = localStorage.getItem("theme");
+    const html = document.documentElement;
+    if (saved === "darkTheme"){
+        html.classList.remove('lightTheme');
+        html.classList.add('darkTheme');
+        localStorage.setItem("theme", "darkTheme");
+        }
+    else {
+        html.classList.remove('darkTheme');
+        html.classList.add('lightTheme');
+        localStorage.setItem("theme", "lightTheme");
+    }
+}
+
+function updateThemeIcons(){
+    const saved = localStorage.getItem("theme");
+    const toglIcon = document.getElementById("themeToggleIcon");
+    const gearIcon = document.getElementById("CECLogo");
+    if(!toglIcon || !gearIcon) return;
+    if(saved === "darkTheme"){
+        toglIcon.src = "/assets/icons/DARKMODE-IconWhite.svg";
+        gearIcon.src = "/assets/icons/CECGearWhite.svg";
+    }
+    else {
+        toglIcon.src = "/assets/icons/DARKMODE-IconBlack.svg";
+        gearIcon.src = "/assets/icons/CECGearBlack.svg";
+    }
+}
+
+function changeTheme(){
+    const html = document.documentElement;
+    const toglIcon = document.getElementById("themeToggleIcon");
+    const gearIcon = document.getElementById("CECLogo");
+
+    if (html.classList.contains("lightTheme")){
+        html.classList.remove('lightTheme');
+        html.classList.add('darkTheme');
+        localStorage.setItem("theme", "darkTheme");
+        toglIcon.src = "/assets/icons/DARKMODE-IconWhite.svg";
+        gearIcon.src = "/assets/icons/CECGearWhite.svg";
+        }
+    else {
+        html.classList.remove('darkTheme');
+        html.classList.add('lightTheme');
+        localStorage.setItem("theme", "lightTheme");
+        toglIcon.src = "/assets/icons/DARKMODE-IconBlack.svg";
+        gearIcon.src = "/assets/icons/CECGearBlack.svg";
+    }
+}
